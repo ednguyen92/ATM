34c015f4107da28eb452cef35923c1a2bcf0e5d6
ednguyen <ednguyen92@gmail.com>
2018-11-01 14:06:13 -0700

    working on layout

---
+++ assets/js/init.js
@@ -0,0 +1,23 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.dropdown-trigger').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      hover: true, // Activate on hover
+      belowOrigin: true, // Displays dropdown below the button
+      alignment: 'right' // Displays dropdown with edge aligned to the left of button
+    });
+    $('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      hover: true, // Activate on hover
+      belowOrigin: true, // Displays dropdown below the button
+      alignment: 'right' // Displays dropdown with edge aligned to the left of button
+    }
+  );
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
