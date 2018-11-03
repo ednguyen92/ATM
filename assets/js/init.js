b0bbf5349680e7f0d1803df2b90bd6e5ff9b7c9d
ednguyen <ednguyen92@gmail.com>
2018-11-02 21:33:16 -0700

    Finished Base Layout

--- assets/js/init.js
+++ assets/js/init.js
@@ -3,21 +3,13 @@
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
+    $('.scrollspy').scrollSpy();
     $('.dropdown-trigger').dropdown({
       inDuration: 300,
       outDuration: 225,
       hover: true, // Activate on hover
-      belowOrigin: true, // Displays dropdown below the button
-      alignment: 'right' // Displays dropdown with edge aligned to the left of button
+      // belowOrigin: true, // Displays dropdown below the button
+      coverTrigger: false
     });
-    $('.dropdown-button').dropdown({
-      inDuration: 300,
-      outDuration: 225,
-      hover: true, // Activate on hover
-      belowOrigin: true, // Displays dropdown below the button
-      alignment: 'right' // Displays dropdown with edge aligned to the left of button
-    }
-  );
-
   }); // end of document ready
 })(jQuery); // end of jQuery name space
