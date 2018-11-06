1d92c0eae9be0549bf59498e874af19254d89ff7
ednguyen <ednguyen92@gmail.com>
2018-11-05 18:11:15 -0800

    90% completion

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,3 +25,13 @@ jQuery(document).ready(function ($) {
 
   });  
 });
+
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true,
+  indicators: true,
+}, setTimeout(autoplay, 4500));
+
+function autoplay() {
+  $('.carousel').carousel('next');
+  setTimeout(autoplay, 4500);
+} 
