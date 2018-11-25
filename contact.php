28cb82dc9fef5843036bcf365125f3c7a4ceedd9
ednguyen <ednguyen92@gmail.com>
2018-11-24 19:39:56 -0800

    .

--- contact.php
+++ contact.php
@@ -1,54 +1,18 @@
-<?php 
-$errors = '';
-$myemail = 'ednguyen92@gmail.com';//<-----Put Your email address here.
-if(empty($_POST['name'])  || 
-   empty($_POST['email']) || 
-   empty($_POST['message']))
-{
-    $errors .= "\n Error: all fields are required";
-}
-
-$first_name = $_POST['first_name'];
-$last_name = $_POST['last_name'];
-$company = $_POST['company_name'];
-$phone_number = $POST['phonenumber']; 
-$email_address = $_POST['email']; 
-$message = $_POST['message']; 
-
-if (!preg_match(
-"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
-$email_address))
-{
-    $errors .= "\n Error: Invalid email address";
-}
-
-if( empty($errors))
-{
-	$to = $myemail; 
-	$email_subject = "Requesting Quote from: $first_name $last_name";
-	$email_body = "You have received a new message. ".
-	" Here are the details:\n Name: $first_name $last_name \n Email: $email_address \n Comapny: $company \n $phone_number \n Message \n $message"; 
-	
-	$headers = "From: $myemail\n"; 
-	$headers .= "Reply-To: $email_address";
-	
-	mail($to,$email_subject,$email_body,$headers);
-	//redirect to the 'thank you' page
-	header('Location: index.html');
-} 
-?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
-<html>
-<head>
-	<title>Contact form handler</title>
-</head>
-
-<body>
-<!-- This page is displayed only if there is some error -->
 <?php
-echo nl2br($errors);
-?>
 
+if (isset($_POST['submit'])) {
+    $firstname = $_POST['first_name'];
+    $lastname = $_POST['last_name'];
+    $companyname = $_POST['company_name'];
+    $phonenumber = $_POST['phonenumber'];
+    $mailFrom = $_POST['email'];
+    $message = $_POST['message'];
 
-</body>
-</html>
+    $mailTo = "ednguyen92@gmail.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have received an email from ".$firstname.".\n\n".$message;
+
+    mail($mailTo, $companyname, $txt, $headers);
+    header("Location: index.html?mailsend");
+
+}
